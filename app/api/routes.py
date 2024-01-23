8f7327a526acfe7ee18a4e6e7acec6a6130cff74
Kristoffer Griessmann <kristoffergriessmann@gmail.com>
2024-01-23 15:39:55 -0800

    updating requirements.txt

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,10 +4,6 @@ from models import db, User, Car, car_schema, cars_schema
 
 api = Blueprint('api',__name__, url_prefix='/api')
 
-@api.route('/getdata')
-def getdata():
-    return {'yee': 'haw'}
-
 @api.route('/cars', methods = ['POST'])
 @token_required
 def create_car(current_user_token):
